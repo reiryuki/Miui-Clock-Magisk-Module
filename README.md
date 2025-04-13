805b7e6b6b10a56c75bc2aa95763ed5dd78bf9b2
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2025-04-13 10:01:00 +0700

    v1.4

--- README.md
+++ README.md
@@ -33,15 +33,15 @@
 - Full screen notification does not launch on top of keyguard in non-AOSP signatured ROM without disabled Android Signature Verification in Android 13 and bellow
 
 ## Optionals
-- https://t.me/androidryukimodsdiscussions/54012
+- https://t.me/ryukinotes/42
 - Global: https://t.me/ryukinotes/35
 
 ## Troubleshootings
-- https://t.me/androidryukimodsdiscussions/64467
+- https://t.me/ryukinotes/19
 - Global: https://t.me/ryukinotes/34
 
 ## Support & Bug Report
-- https://t.me/androidryukimodsdiscussions/2618
+- https://t.me/ryukinotes/54
 - If you don't do above, issues will be closed immediately
 
 ## Credits and Contributors
--- service.sh
+++ service.sh
@@ -29,20 +29,7 @@ else
 fi
 
 # function
-grant_permission() {
-pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
-pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
-if [ "$API" -ge 29 ]; then
-  pm grant $PKG android.permission.ACCESS_MEDIA_LOCATION 2>/dev/null
-  appops set $PKG ACCESS_MEDIA_LOCATION allow
-fi
-if [ "$API" -ge 33 ]; then
-  pm grant $PKG android.permission.READ_MEDIA_AUDIO
-  pm grant $PKG android.permission.READ_MEDIA_VIDEO
-  pm grant $PKG android.permission.READ_MEDIA_IMAGES
-  pm grant $PKG android.permission.POST_NOTIFICATIONS
-  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
-fi
+appops_set() {
 appops set $PKG LEGACY_STORAGE allow
 appops set $PKG READ_EXTERNAL_STORAGE allow
 appops set $PKG WRITE_EXTERNAL_STORAGE allow
@@ -52,6 +39,9 @@ appops set $PKG READ_MEDIA_IMAGES allow
 appops set $PKG WRITE_MEDIA_AUDIO allow
 appops set $PKG WRITE_MEDIA_VIDEO allow
 appops set $PKG WRITE_MEDIA_IMAGES allow
+if [ "$API" -ge 29 ]; then
+  appops set $PKG ACCESS_MEDIA_LOCATION allow
+fi
 if [ "$API" -ge 30 ]; then
   appops set $PKG MANAGE_EXTERNAL_STORAGE allow
   appops set $PKG NO_ISOLATED_STORAGE allow
@@ -60,8 +50,11 @@ fi
 if [ "$API" -ge 31 ]; then
   appops set $PKG MANAGE_MEDIA allow
 fi
+if [ "$API" -ge 33 ]; then
+  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
+fi
 if [ "$API" -ge 34 ]; then
-  appops set $PKG READ_MEDIA_VISUAL_USER_SELECTED allow
+  appops set "$PKG" READ_MEDIA_VISUAL_USER_SELECTED allow
 fi
 PKGOPS=`appops get $PKG`
 UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
@@ -81,17 +74,15 @@ fi
 
 # grant
 PKG=com.miui.deskclock
-pm grant $PKG android.permission.READ_PHONE_STATE
-appops set $PKG SYSTEM_ALERT_WINDOW allow
-appops set $PKG TURN_SCREEN_ON allow
-if [ "$API" -ge 34 ]; then
-  appops set $PKG USE_FULL_SCREEN_INTENT allow
+if appops get $PKG > /dev/null 2>&1; then
+  pm grant --all-permissions $PKG
+  appops set $PKG SYSTEM_ALERT_WINDOW allow
+  appops set $PKG TURN_SCREEN_ON allow
+  if [ "$API" -ge 34 ]; then
+    appops set $PKG USE_FULL_SCREEN_INTENT allow
+  fi
+  appops_set
 fi
-grant_permission
-if appops get com.qualcomm.qti.poweroffalarm > /dev/null 2>&1; then
-  pm grant $PKG org.codeaurora.permission.POWER_OFF_ALARM 2>/dev/null
-fi
-
 
 
 
